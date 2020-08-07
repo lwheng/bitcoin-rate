2076326122d0a62212631910f88d64d438c6e2e2
Low-Wee Heng <alpha116@gmail.com>
2020-08-07 11:49:45 +0800

    Elm can communicate with our HTTP server

--- web/src/Main.elm
+++ web/src/Main.elm
@@ -3,23 +3,64 @@ module Main exposing (..)
 import Browser
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
+import Http
+
 
 main =
-  Browser.sandbox { init = 0, update = update, view = view }
+    Browser.element { init = init, update = update, view = view, subscriptions = subscriptions }
 
-type Msg = Increment | Decrement
 
+
+-- MODEL
+
+
+type alias Model =
+    String
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( "Please click Reload", Cmd.none )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = GotRate (Result Http.Error String)
+    | Reload
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    Increment ->
-      model + 1
+    case msg of
+        Reload ->
+            ( model, getRate )
 
-    Decrement ->
-      model - 1
+        GotRate (Err error) ->
+            ( Debug.toString error, Cmd.none )
 
+        GotRate (Ok val) ->
+            ( val, Cmd.none )
+
+
+view : Model -> Html Msg
 view model =
-  div []
-  [ button [ onClick Decrement ] [ text "-" ]
-  , div [] [ text (String.fromInt model) ]
-  , button [ onClick Increment ] [ text "+" ]
-  ]
+    div []
+        [ div [] [ text model ]
+        , button [ onClick Reload ] [ text "Reload" ]
+        ]
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
+
+
+getRate : Cmd Msg
+getRate =
+    Http.get
+        { url = "http://localhost:3000/bitcoin-rate"
+        , expect = Http.expectString GotRate
+        }
