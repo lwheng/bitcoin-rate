1298d1fb0fd0901f39da6e2ecd52c7150c4ff606
Low-Wee Heng <alpha116@gmail.com>
2020-08-13 10:34:13 +0800

    Switch to Browser.application

--- web/src/Main.elm
+++ web/src/Main.elm
@@ -1,13 +1,22 @@
-module Main exposing (..)
+module Main exposing (main)
 
-import Browser
-import Html exposing (Html, button, div, text)
+import Browser as Browser
+import Browser.Navigation as Navigation
+import Html exposing (..)
 import Html.Events exposing (onClick)
 import Http
+import Url exposing (..)
 
 
 main =
-    Browser.element { init = init, update = update, view = view, subscriptions = subscriptions }
+    Browser.application
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        , onUrlRequest = onUrlRequest
+        , onUrlChange = onUrlChange
+        }
 
 
 
@@ -18,9 +27,23 @@ type alias Model =
     String
 
 
-init : () -> ( Model, Cmd Msg )
-init _ =
+init : () -> Url -> Navigation.Key -> ( Model, Cmd Msg )
+init _ _ _ =
     ( "Please click Reload", Cmd.none )
+
+
+
+-- VIEW
+
+
+view : Model -> Browser.Document Msg
+view model =
+    { title = "Sample"
+    , body =
+        [ div [] [ text model ]
+        , button [ onClick Reload ] [ text "Reload" ]
+        ]
+    }
 
 
 
@@ -45,17 +68,31 @@ update msg model =
             ( val, Cmd.none )
 
 
-view : Model -> Html Msg
-view model =
-    div []
-        [ div [] [ text model ]
-        , button [ onClick Reload ] [ text "Reload" ]
-        ]
+
+-- SUBSCRIPTIONS
 
 
 subscriptions : Model -> Sub Msg
 subscriptions _ =
     Sub.none
+
+
+
+-- onUrlRequest
+
+
+onUrlRequest : Browser.UrlRequest -> Msg
+onUrlRequest _ =
+    Reload
+
+
+
+-- onUrlChange
+
+
+onUrlChange : Url -> Msg
+onUrlChange _ =
+    Reload
 
 
 getRate : Cmd Msg
